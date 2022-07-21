8b70f39b2ffb622a96289095abfdb43db28d1164
YanaKramareva <iana2000@mail.ru>
2022-07-21 17:49:42 +0300

    changed app.blade.php, index.blade.php

--- resources/views/labels/index.blade.php
+++ resources/views/labels/index.blade.php
@@ -3,32 +3,18 @@
 @section('content')
     <div class="container">
         <h1 class="mb-5">{{ __('labels.Labels') }}</h1>
-        @if(Auth::check())
-            <a href="{{ route('labels.create') }}" class="btn btn-info">{{ __('labels.Create label') }}</a>
-        @endif
         <table class="table mt-2">
             <thead>
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col"  class="text-break">{{ __('labels.Label name') }}</th>
-                <th scope="col">{{ __('labels.Date of creation') }}</th>
-                @if(Auth::check())
-                    <th scope="col">{{ __('labels.Actions') }}</th>
-                @endif
-            </tr>
+             </tr>
             </thead>
             @if ($labels)
                 @foreach ($labels as $label)
                     <tr>
                         <td>{{ $label->id }}</td>
                         <td scope="row"> {{ $label->name }} </td>
-                        <td>{{ $label->created_at->format('d.m.Y') }}</td>
-                        @if(Auth::check())
-                            <td>
-                                <a class="text-danger" href="{{ route('labels.destroy', ['label' => $label]) }}" data-method="delete" rel="nofollow" data-confirm="{{ __('labels.Are you sure?') }}">{{ __('labels.Delete') }}</a>
-                                <a href="{{ route('labels.edit', ['label' => $label]) }}">{{ __('labels.Edit') }}</a>
-                            </td>
-                        @endif
                     </tr>
                 @endforeach
             @endif
